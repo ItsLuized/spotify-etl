454e6e71536c206d75d11293f74bd1ea3da3501a
Luis Martinez <luisemartinezcruz@gmail.com>
2022-10-04 15:23:19 -0300

    Validation and Load stages complete + pep8 styles

--- main.py
+++ main.py
@@ -9,45 +9,110 @@ import sqlite3
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER_ID = "itsluized"
-TOKEN = "BQDrOt6C5ruH31E1CrAjEuYLJaJ267Gj2utquesxRPHtFOnr_W1iKYldpCZQp1eUJN2bYVbNygGF_otpqrZYbdu0lQDvImQ_b801HwnKp54R9oMYZeeQ7O_NCA0feLF90ToWHy9tFZIMyt5XbMU"
+TOKEN = "BQALLFz-wQhwOIcqCXbCTTEvw2Rk0PWD7HnB2ArvUbjbnO2u_yotFS6pFXm2J-MQCw0T9SY7l-VbFztaMzS_9kepn-IsWNKtV-6CasCGRtsBkO2QObK4MvY48B-vjx-mskRW6FRarbZsbpilk3l0O5pa8mPhy33275sdGqG_EL1JYK2IXd4"
 
-# To get tocken, go to: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbF96VWJrNHRNVXlyU3ZsZnVhYURJMl9ta1pVd3xBQ3Jtc0ttTEw0Sm8xTEFHSVJnVXFMWGktUTMtLWVod1VMbmJSSk9GNHdGRHRsOWc2cl91ZlhTODJ3ZE14NjJEOEVuTXJ5M2ZlbkFodzQ1MGljcFl0M2xrc3RRY2xBRmlRbFpiRUR6VjBucTZtUjFzX0toT0E2RQ&q=https%3A%2F%2Fdeveloper.spotify.com%2Fconsole%2Fget-recently-played%2F&v=dvviIUKwH7o"
+# To get token, go to: "https://developer.spotify.com/console/get-recently-played/"
+
+
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False
+
+    # Primary Key Check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key Check is violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null value found")
+
+    #  Check that all timestamps are of yesterday's date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].to_list()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception(
+                "At least one of the returned songs doesnot come from within the last 24 hours")
+
+    return True
+
 
 if __name__ == "__main__":
 
-  # EXTRACT PART OF ETL
-  headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization" : "Bearer {token}".format(token=TOKEN)
-  }
+    # EXTRACT PART OF ETL
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {token}".format(token=TOKEN)
+    }
 
-  today = datetime.datetime.now()
-  yesterday = today - datetime.timedelta(days=1)
-  yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
-  r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp),headers=headers)
+    r = requests.get(
+        "https://api.spotify.com/v1/me/player/recently-played?after={time}".format(
+            time=yesterday_unix_timestamp),
+        headers=headers)
 
-  data = r.json()
+    data = r.json()
 
-  song_names = []
-  artist_names = []
-  played_at_list = []
-  timestamps = []
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
 
-  for song in data["items"]:
-    song_names.append(song["track"]["name"])
-    artist_names.append(song["track"]["album"]["artists"][0]["name"])
-    played_at_list.append(song["played_at"])
-    timestamps.append(song["played_at"][0:10])
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at_list.append(song["played_at"])
+        timestamps.append(song["played_at"][0:10])
 
-  song_dict = {
-    "song_name": song_names,
-    "artist_name": artist_names,
-    "played_at": played_at_list,
-    "timestamp": timestamps
-  }
+    song_dict = {
+        "song_name": song_names,
+        "artist_name": artist_names,
+        "played_at": played_at_list,
+        "timestamp": timestamps
+    }
 
-  song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+    song_df = pd.DataFrame(
+        song_dict,
+        columns=["song_name", "artist_name", "played_at", "timestamp"])
 
-  print(song_df)
+    # Validate
+    if check_if_valid_data(song_df):
+        print("Data valid, proceed to Load stage")
+
+    # Load
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists="append")
+    except:
+        print("Data already exists in the database")
+
+    conn.close()
+    print("Close database successfully")
+
+    # Job Scheduling
+    # ...
